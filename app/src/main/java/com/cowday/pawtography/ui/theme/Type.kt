5fba44bf4291293a3a51a078fa71206eca23a0ad
shantanu1k <shantanukawade2@gmail.com>
2025-02-21 16:34:49 +0530

    Update App Typography

--- app/src/main/java/com/cowday/pawtography/ui/theme/Type.kt
+++ app/src/main/java/com/cowday/pawtography/ui/theme/Type.kt
@@ -2,33 +2,78 @@ package com.cowday.pawtography.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.cowday.pawtography.R
+
+val sfProFontFamily = FontFamily(
+    Font(R.font.sf_pro_regular, FontWeight.Normal),
+    Font(R.font.sf_pro_medium, FontWeight.Medium),
+    Font(R.font.sf_pro_bold, FontWeight.Bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+    headlineLarge = TextStyle(
+        fontFamily = sfProFontFamily,
+        fontSize = 34.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = sfProFontFamily,
+        fontSize = 28.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = sfProFontFamily,
         fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontWeight = FontWeight.Bold
+    ),
+    titleLarge = TextStyle(
+        fontFamily = sfProFontFamily,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    titleMedium = TextStyle(
+        fontFamily = sfProFontFamily,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    titleSmall = TextStyle(
+        fontFamily = sfProFontFamily,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = sfProFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = sfProFontFamily,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    bodySmall = TextStyle(
+        fontFamily = sfProFontFamily,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    labelLarge = TextStyle(
+        fontFamily = sfProFontFamily,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Light
+    ),
+    labelMedium = TextStyle(
+        fontFamily = sfProFontFamily,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Light
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = sfProFontFamily,
+        fontSize = 10.sp,
+        fontWeight = FontWeight.Light
     )
-    */
 )
