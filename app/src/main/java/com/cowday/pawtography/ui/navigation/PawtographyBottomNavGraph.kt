31a51fa0658549c13bbbeee2b297f1425c47de40
shantanu1k <shantanukawade2@gmail.com>
2025-02-22 16:39:25 +0530

    Add Screen Navigation Transitions

--- app/src/main/java/com/cowday/pawtography/ui/navigation/PawtographyBottomNavGraph.kt
+++ app/src/main/java/com/cowday/pawtography/ui/navigation/PawtographyBottomNavGraph.kt
@@ -1,5 +1,7 @@
 package com.cowday.pawtography.ui.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -21,14 +23,52 @@ fun PawtographyNavGraph(
         startDestination = PawtographyNavScreen.HOME.route,
         modifier = modifier,
     ) {
-        composable(route = PawtographyNavScreen.HOME.route) {
+        composable(
+            route = PawtographyNavScreen.HOME.route,
+            enterTransition = {
+                slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Right,
+                    tween(300)
+                )
+            }
+        ) {
             HomeScreen(navController)
         }
-        composable(route = PawtographyNavScreen.GENERATE.route) {
+        composable(
+            route = PawtographyNavScreen.GENERATE.route,
+            enterTransition = {
+                slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    tween(SLIDING_TRANSITION_DURATION_IN_MILLIS)
+                )
+            },
+            popExitTransition = {
+                slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Right,
+                    tween(SLIDING_TRANSITION_DURATION_IN_MILLIS)
+                )
+            }
+        ) {
             GenerateScreen(navController, viewModel)
         }
-        composable(route = PawtographyNavScreen.CACHE.route) {
-            CacheScreen(navController)
+        composable(
+            route = PawtographyNavScreen.CACHE.route,
+            enterTransition = {
+                slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    tween(SLIDING_TRANSITION_DURATION_IN_MILLIS)
+                )
+            },
+            popExitTransition = {
+                slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Right,
+                    tween(SLIDING_TRANSITION_DURATION_IN_MILLIS)
+                )
+            }
+        ) {
+            CacheScreen(navController, viewModel)
         }
     }
 }
+
+private const val SLIDING_TRANSITION_DURATION_IN_MILLIS = 400
