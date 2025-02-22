44ffdfa1e9fd4cb15af10003cdddce8d770fbbb6
shantanu1k <shantanukawade2@gmail.com>
2025-02-22 16:40:09 +0530

    Use Response

--- app/src/main/java/com/cowday/pawtography/network/DogApi.kt
+++ app/src/main/java/com/cowday/pawtography/network/DogApi.kt
@@ -1,9 +1,10 @@
 package com.cowday.pawtography.network
 
 import com.cowday.pawtography.models.network.DogResponse
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface DogApi {
     @GET("api/breeds/image/random")
-    suspend fun getRandomDogImage(): DogResponse
+    suspend fun getRandomDogImage(): Response<DogResponse>
 }
