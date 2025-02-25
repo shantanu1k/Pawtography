dfaa866a52f16bbd82ccced9eb4bce50ccd99727
shantanu1k <shantanukawade2@gmail.com>
2025-02-25 21:07:09 +0530

    Disable dynamic theme
    Prevents the app theme being overriden by dynamic theme.

--- app/src/main/java/com/cowday/pawtography/ui/theme/Theme.kt
+++ app/src/main/java/com/cowday/pawtography/ui/theme/Theme.kt
@@ -30,7 +30,7 @@ private val LightColorScheme = lightColorScheme(
 fun PawtographyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
